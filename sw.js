27c8b76962e1db5f1720590276ee9eec33a24e7e
Fabián Alexis Peña Peña <fabianale3@gmail.com>
2026-07-12 04:00:14 -0400

    pomo_elegido por usuario

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'pomodoro-amor-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './estilos.css',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caché abierto correctamente.');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Borrando caché antiguo viejo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Devuelve desde el caché si está guardado
+        }
+        return fetch(event.request); // Si no, lo busca en internet
+      })
+  );
+});
