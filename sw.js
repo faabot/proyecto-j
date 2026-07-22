25b5edfb4c14fd02eeb9c5d90527bf89c66d0eab
Fabián Alexis Peña Peña <fabianale3@gmail.com>
2026-07-22 15:59:16 -0400

    version 3.4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cámbiale el nombre UNA ÚLTIMA VEZ para que el celular borre el viejo e instale este nuevo
-const CACHE_NAME = 'pomodoro-amor-v-FINAL.3.3.3'; 
+const CACHE_NAME = 'pomodoro-amor-v-FINAL.3.4'; 
 const urlsToCache = [
   './',
   './index.html',
